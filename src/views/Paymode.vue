392325ac446978cd0e024c60c040adeb46ceb982
Alexander Hurtado <alexander.hurtado7170@gmail.com>
2023-11-23 22:14:17 -0500

    Paymode index

--- src/views/Paymode.vue
+++ src/views/Paymode.vue
@@ -1,21 +1,36 @@
 <template>
-    <div class="container">    
-    <h1>Pay Mode List</h1>    
+    <div class="container"> 
+    <h1 class="text-start">Paymode List |
+        <button @click="newPaymode()"  
+        class="btn btn-success mx-2">
+        <font-awesome-icon icon="plus" />
+        </button>
+    </h1>
 <table class="table">
     <thead>
         <tr>
             <th scope="col">Id</th>
             <th scope="col">Name</th>
-            <th scope="col">Observation</th>           
+            <th scope="col">Observation</th> 
+            <th scope="col">Actions</th>             
         </tr>
     </thead>
     <tbody>       
-        <tr>
-            <th v-for="(paymode, index)in paymodes" :key="index">
-            <th scope="row">{{ index +1 }}</th>
+        <tr v-for="(paymode, index) in paymodes" :key="index">
+            <th scope="row">{{ index + 1 }}</th>
             <td>{{ paymode.id }}</td>
             <td>{{ paymode.name }}</td>
             <td>{{ paymode.observation }}</td>
+            <td>
+                <button @click="deletePaymode(paymode.id)"
+                    class="btn btn-danger mx-2">
+                    <font-awesome-icon icon="trash" />
+                </button> 
+                <button @click="editPaymode(paymode.id)"           
+                class="bnt bnt-warning mx-2">
+                <font-awesome-icon icon="pencil" />
+                </button> 
+            </td>
         </tr>
         </tbody>
     </table>
